78dd6bf7bf2e364d1fe9ed589beb09a60b80bd4c
MicDonald <13977688+MicDonald@users.noreply.github.com>
2021-09-14 15:45:08 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express();
+var port = 3000;
+var fetch = require('node-fetch');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/buses', async (req, res) => {
+  var url ='https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+  var response = await fetch(url);
+  var json = await response.json();
+  res.send(json.data);
+})
+
+app.listen(port, () => {
+  console.log('Example app listening at http://localhost:${port}')
+})
